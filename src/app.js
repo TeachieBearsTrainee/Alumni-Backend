e44d08f4cce93440575f96d1686104b5428fbeeb
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-03-28 18:38:22 +0530

    updated cors 2

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://alumni-client-rajendra.vercel.app"
+    "https://alumni-client-rajendra.vercel.app",
+    "https://alumni-client-delta.vercel.app"
 ];
 
 app.use(cors({
